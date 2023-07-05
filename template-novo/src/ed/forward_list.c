ab459f7b296eadf29d5235109b674b495dd57a5b
AntonioFerreira <axd108@gmail.com>
2023-07-05 16:01:09 -0300

    alterações na forward list

--- template-novo/src/ed/forward_list.c
+++ template-novo/src/ed/forward_list.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "types.h"
 #include "forward_list.h"
 
 // TODO!
@@ -43,7 +44,20 @@ int forward_list_size(ForwardList *l){
     return l->size;
 }
 
-data_type forward_list_find(ForwardList *l, void *key, int (*cmp_fn)(data_type data, void *key));
+data_type forward_list_find(ForwardList *l, void *key, int (*cmp_fn)(data_type data, void *key)){
+    Node *iterator = l->head;
+    while (iterator != NULL)
+    {
+        if(cmp_fn(iterator->value,key)){
+            
+        }
+    }
+    
+}
+
+void forward_list_refresh(ForwardList *l,void *key, data_type value){
+
+}
 
 void forward_list_push_front(ForwardList *l, data_type data);
 
@@ -57,20 +71,39 @@ data_type forward_list_pop_front(ForwardList *l);
 
 ForwardList *forward_list_reverse(ForwardList *l);
 
-void forward_list_clear(ForwardList *l);
+void forward_list_clear(ForwardList *l){
+
+}
 
 void forward_list_remove(ForwardList *l, data_type val,int (*cmpFn)(data_type, data_type)){
     Node *iterator = l->head;
+    Node *prev = NULL;
+
     Node *last = NULL;
-    int cycle = 0;
-    while (iterator != NULL)
+    if (l->head == NULL)
     {
-       
-        iterator = iterator->next;
-        cycle++;
+        return;
     }
-    
+    else if (cmpFn(iterator->value,val))
+    {
+        l->head = l->head->next;
+        node_destroy(iterator);
+    }
+    else
+    {
+          while (iterator != NULL)
+             {
+                if(cmpFn(iterator->value,val)){
+                    prev->next = iterator->next;
+                    node_destroy(iterator);
+                }
+                prev = iterator;
+                iterator = iterator->next;
+            }
+    }
 }
 
-void forward_list_destroy(ForwardList *l);
+void forward_list_destroy(ForwardList *l){
+    
+}
 
